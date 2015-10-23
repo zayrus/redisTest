d2ea5cf841a8286d4c06b99261ead98f9897f70d
evenencio <evenencio@edrans.com>
2015-10-23 14:45:46 -0300

    add redis basic test

--- server.js
+++ server.js
@@ -1,31 +1,48 @@
 var express = require("express");
 var app = express();
+
 var router = express.Router();
+
 var _dirname = "/Users/mbianco/redisTest";
 var path = _dirname + "/views/";
 
-    router.use(function (req,res,next){
-        console.log("/"+req.method);
-        next();
-    })
+var redis = require("redis");
+var client = redis.createClient();
+    
+client.on('connect', function(){
+    console.log("connected")
+});
 
-    router.get("/",function(req,res){
-        res.sendFile( path + "index.html")
-    });
+//example set 
+//client.set('framework', 'AngularJS');
 
-    router.get("/about",function(req,res){
-        res.sendFile( path + "about.html")
-    });
-    router.get("/contact",function(req,res){
-        res.sendFile( path + "contact.html")
-    });
+router.use(function (req,res,next){
+    console.log("/"+req.method);
+    next();
+})
 
-    app.use("/",router);
+router.get("/",function(req,res){
+    res.sendFile( path + "index.html")
+});
 
-    app.use("*",function(req,res){
-        res.sendFile(path +"404.html");
-    });
+router.get("/about",function(req,res){
+    res.sendFile( path + "about.html")
+});
+router.get("/contact",function(req,res){
+    res.sendFile( path + "contact.html")
+});
 
-    app.listen(3000,function(){
-        console.log("working on port 3000");
-    });
+app.use("/",router);
+
+app.use("*",function(req,res){
+    res.sendFile(path +"404.html");
+});
+
+//Example Get
+/*client.get('framework', function(err, reply) {
+    console.log(reply);
+});*/
+
+app.listen(3000,function(){
+    console.log("working on port 3000");
+});
